31f6af08e782895557901e23ffdeabf3ec349944
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-03-25 00:20:24 -0300

    refactor: project is now organized according to clean architecture

---
+++ TheatricalPlayersRefactoringKata.Domain/Models/Performance.cs
@@ -0,0 +1,16 @@
+namespace TheatricalPlayersRefactoringKata.Domain.Models;
+
+public class Performance
+{
+    private string _playId;
+    private int _audience;
+
+    public string PlayId { get => _playId; set => _playId = value; }
+    public int Audience { get => _audience; set => _audience = value; }
+
+    public Performance(string playID, int audience)
+    {
+        _playId = playID;
+        _audience = audience;
+    }
+}
