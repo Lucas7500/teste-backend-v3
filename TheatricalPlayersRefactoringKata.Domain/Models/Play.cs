09d2b234ef4f0e8d5c08924c141dad8b9508648f
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-03-26 02:57:17 -0300

    refactor: changing type of "type" property in Play class

--- TheatricalPlayersRefactoringKata.Domain/Models/Play.cs
+++ TheatricalPlayersRefactoringKata.Domain/Models/Play.cs
@@ -1,16 +1,18 @@
+using TheatricalPlayersRefactoringKata.Domain.Enums;
+
 namespace TheatricalPlayersRefactoringKata.Domain.Models;
 
 public class Play
 {
     private string _name;
     private int _lines;
-    private string _type;
+    private GenreEnum _type;
 
     public string Name { get => _name; set => _name = value; }
     public int Lines { get => _lines; set => _lines = value; }
-    public string Type { get => _type; set => _type = value; }
+    public GenreEnum Type { get => _type; set => _type = value; }
 
-    public Play(string name, int lines, string type) {
+    public Play(string name, int lines, GenreEnum type) {
         _name = name;
         _lines = lines;
         _type = type;
