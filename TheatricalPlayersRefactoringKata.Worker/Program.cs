e9e3f891f3a88117085bd971da98fbda6e565cbe
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-03-26 02:59:24 -0300

    remove: removing worker project

--- TheatricalPlayersRefactoringKata.Worker/Program.cs
+++
@@ -1,7 +0,0 @@
-using TheatricalPlayersRefactoringKata.Worker;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
